256874b8b3c4b679649376b7006a0860a5eafb89
Nicolás Montiel <nicolasmontielf@gmail.com>
2023-03-15 00:42:45 -0300

    Added Blog post policy

---
+++ app/Policies/BlogPostPolicy.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\BlogPost;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class BlogPostPolicy
+{
+    use HandlesAuthorization;
+
+    public function before(User $user): bool|null {
+        if ($user->isSuperAdmin()) {
+            return true;
+        }
+        return null;
+    }
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, BlogPost $blogPost): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user): bool
+    {
+        return $user->hasPermissions('blog_post.create');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, BlogPost $blogPost): bool
+    {
+        if ($user->hasPermissions('blog_post.update')) {
+            return true;
+        }
+
+        if ($user->hasPermissions('blog_post.update.own') && $user->id === $blogPost->user_id) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, BlogPost $blogPost)
+    {
+        if ($user->hasPermissions('blog_post.delete')) {
+            return true;
+        }
+
+        if ($user->hasPermissions('blog_post.delete.own') && $user->id === $blogPost->user_id) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, BlogPost $blogPost)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, BlogPost $blogPost)
+    {
+        if ($user->hasPermissions('blog_post.delete')) {
+            return true;
+        }
+
+        if ($user->hasPermissions('blog_post.delete.own') && $user->id === $blogPost->user_id) {
+            return true;
+        }
+
+        return false;
+    }
+}
